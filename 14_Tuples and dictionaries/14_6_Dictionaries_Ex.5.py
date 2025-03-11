f04fd0e6fdd4d428274232e1ce136c83cba26de8
Wioletta <wiolmette@gmail.com>
2025-03-11 15:07:01 +0100

    adding a part of an exercise

--- 14_Tuples and dictionaries/14_6_Dictionaries_Ex.5.py
+++ 14_Tuples and dictionaries/14_6_Dictionaries_Ex.5.py
@@ -23,7 +23,9 @@ def create():
     telefon = input("Telefon kontaktu: ")
     email = input("E-mail kontaktu: ")
     # print(imie_nazwisko)
+    global telefon1 
     telefon1 = {"telefon": telefon}
+    global email1 
     email1 = {"email": email}
     contacts[imie_nazwisko] = telefon1, email1
     print(contacts)
@@ -39,3 +41,28 @@ def read():
 read()
 
 # ------------------------------------
+
+# ZROBIC ZADANIE ZAGNIEZDZAJAC SLOWNIKI A NIE PISZAC KLUCZE JAKO STRINGI <STUPIDO> XD 
+# DODAC WARUNKI CO W RAZIE BLEDNEGO NAPISANIA NP. IMIENIA CZY COS
+
+def update():
+    kontakt2 = input("Wprowadz imie i nazwisko kontaktu, ktory chcesz zaktualizowac: ")
+    wybor_opcji = input("Czy chcesz zaktualizowac numer telefonu? Napisz 'tak' lub 'nie'. ")
+    if wybor_opcji == "tak":    
+        telefon2 = input("Telefon kontaktu: ")
+        telefon3 = {"telefon": telefon2}
+        contacts[kontakt2] = telefon3, email1
+        wybor_opcji2 = input("Czy chcesz zaktualizowac rowniez adres email? Napisz 'tak' lub 'nie'. ")
+        if wybor_opcji2 == "tak":
+            email2 = input("E-mail kontaktu: ")
+            email3 = {"email": email2}
+            contacts[kontakt2] = telefon3, email3
+    if wybor_opcji == "nie":
+        wybor_opcji3 = input("Czy chcesz zaktualizowac adres email? Napisz 'tak' lub 'nie'. ") 
+        if wybor_opcji3 == "tak":
+            email2 = input("E-mail kontaktu: ")
+            email3 = {"email": email2}
+            contacts[kontakt2] = telefon1, email3
+    print(contacts)
+
+update()
