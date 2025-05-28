6659005b395a8bb441da15bc796dacee89b11a6b
Wioletta <wiolmette@gmail.com>
2025-05-28 14:50:33 +0200

    SQL continuation

--- SQL/SQLite_8_Summary2.py
+++ SQL/SQLite_8_Summary2.py
@@ -3,8 +3,8 @@ conn = sqlite3.connect('baza.db')
 cursor = conn.cursor()
 
 # Zadanie typu 10 z lekcji 7, czyli gdy chcę użyć funkcji agregujących i jednocześnie wyświetlić 
-
 # konkretny wiersz (np. nazwę), a grupuję po czymś innym (np. gatunku)
+
 # Dla każdego kraju (BillingCountry) pokaż numer faktury (InvoiceId) o najniższej wartości (Total).
 
 cursor.execute("""SELECT invoices.BillingCountry, invoices.InvoiceId, invoices.Total
@@ -22,4 +22,13 @@ print(results1)
 
 # Zadania dodatkowe - dalsza nauka:
 
-# 
+# 1. Wyświetl tytuły utworów z albumów artysty o nazwie „AC/DC”.
+
+cursor.execute("""SELECT tracks.Name, albums.Title
+               FROM tracks
+               JOIN albums ON albums.AlbumId = tracks.AlbumId
+               JOIN artists ON albums.ArtistId = artists.ArtistId
+               WHERE artists.ArtistId = 1
+               """)
+results2 = cursor.fetchall()
+print(results2)
