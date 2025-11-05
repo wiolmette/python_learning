ff64dff7ba0a559e158a96ecc974671f159a5696
Wioletta <wiolmette@gmail.com>
2025-11-05 14:44:30 +0100

    pandas - continuation

--- 18_PANDAS/5pandas_cleaning_data.py
+++ 18_PANDAS/5pandas_cleaning_data.py
@@ -1,10 +1,5 @@
-
 import pandas as pd
 
-# jak skrocic plik json do 30 wierszy?
+df = pd.read_csv('data2.csv')
 
-df = pd.read_json('data.json')
-
-new_df = df.dropna()        # usuwanie wierszy zawierajacych puste komorki
-
-print(new_df.to_string())
+print(df.head(32))
